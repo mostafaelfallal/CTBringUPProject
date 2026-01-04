fa51b716351e0670a04292b347d7998aafabbb05
Mostafa ElFallal <mostafa.elfallal@coretech-innovations.com>
2026-01-04 12:40:35 +0200

    Docs are updated

--- Client/main.cpp
+++ Client/main.cpp
@@ -9,24 +9,6 @@ int main(int argc, char *argv[])
 
     Client client;
     client.connectToServer();
-
-    // // Setup Threading
-    // QThread workerThread;
-    // InputWorker* worker = new InputWorker;
-    // worker->moveToThread(&workerThread);
-
-    // Connect signals
-    // QObject::connect(&workerThread, &QThread::started, worker, &InputWorker::process);
-    // QObject::connect(worker, &InputWorker::userInputReceived, &client, &Client::sendData);
-
-    // Cleanup hooks
-    // QObject::connect(&workerThread, &QThread::finished, worker, &QObject::deleteLater);
     QObject::connect(&client, &Client::disconnected, &a, &QCoreApplication::quit);
     a.exec();
-    // workerThread.start();
-
-    // int result = a.exec();
-    // workerThread.quit();
-    // workerThread.wait();
-    // return result;
 }
